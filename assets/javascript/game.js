8710ab4e2ab1c0fe7e0dbed2459e21af44fcc4ac
K Uyeda <kellikells@gmail.com>
2020-01-17 15:16:17 -0800

    updating files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,113 +2,96 @@
 // once the <body> loads, it runs this function
 function pageElementsFirst() {
 
-// variables for game stats
-let wins = 0;
-let guessesLeft = 15;
+    // variables for game stats
+    let wins = 0;
+    let guessesLeft = 15;
+
+    // arrays to hold blanks & correct guesses & wrong guesses
+    let hiddenArray = [];
+    let guessesSoFar = [];
 
 
-// arrays to hold blanks & correct guesses & wrong guesses
-let blanks = [];
-// let hiddenArray = [];
-let guessesSoFar = [];
+    // array of all letters that could be part of the answer
+    const alphabet = ["a", "b", "c", "d", "e", "f",
+        "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// connect to HTML so stats are viewable
-let winsText = document.getElementById("wins");
-let guessesLeftText = document.getElementById("guesses-left");
-let guessesSoFarText = document.getElementById("guesses-so-far");
-let blanksText = document.getElementById("blanks");
+    const answerChoices = ['rick', 'morty', 'beth', 'summer',
+        'jerry', 'pencilvester', 'sleepygary', 'misterbeauregard'];
 
-// html display
-
-winsText.textContent = "Wins: " + wins;
-guessesLeftText.textContent = "Guesses left: " + guessesLeft;
-guessesSoFarText.textContent = "Guesses So far: " + guessesSoFar;
-blanksText.textContent = blanks;
-
-
-// document.getElementById("win").innerHTML =winsText;
-
-
-// array of all letters that could be part of the answer
-const alphabet = ["a", "b", "c", "d", "e", "f",
-    "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-const answerChoices = ['rick', 'morty', 'beth', 'summer',
-    'jerry', 'pencilvester', 'sleepygary', 'misterbeauregard'];
-
-
-// ---------------FUNCTION------
-//funcion to generate random computer choice in var, answer
-//---- call this function when-----document load,-- loss,-- win------
-const answer = function () {
-    let word = answerChoices[Math.floor(Math.random() * answerChoices.length)];
-    return word;
-}
-let finalAnswer = answer();
-
-function displayBlankSpaces() {
-    for (let i = 0; i < finalAnswer.length; i++) {
-        blanks.push("__ ");
+    // ---------------FUNCTION------
+    //funcion to generate random computer choice in var, answer
+    //---- call this function when-----document load,-- loss,-- win------
+    let answer = function () {
+        let word = answerChoices[Math.floor(Math.random() * answerChoices.length)];
+        return word;
     }
-    console.log(blanks);
-}
-//---------- function to put correct Letter in place
-let placeLetter = function() {
-    let answerSplit = finalAnswer.split(""); 
-    let letterIndexInAnswer = answerSplit.indexOf(userGuess);
-    let hiddenArray = blanks;
-    
-    hiddenArray.splice(letterIndexInAnswer, 1, userGuess);
+    let finalAnswer = answer();
 
-    document.querySelector('.outputArray').textContent = hiddenArray;
+    let displayBlankSpaces = function () {
+        let blankSpaces =[];
+        for (let i = 0; i < finalAnswer.length; i++) {
+            blankSpaces.push("__ ");
+        }
+        console.log(hiddenArray);
+    }
+       
 
-}
-
-
-
-// ON DOCUMENT LOAD - RUN THIS FUNCTION TO SET UP FIRST WORD
-answer();
-(console.log("HERE IS THE FINAL ANSWER " + finalAnswer));
-displayBlankSpaces();
-console.log("here are the blanks for this round" + blanks);
+    // ON DOCUMENT LOAD - RUN THIS FUNCTION TO SET UP FIRST WORD
+    answer();
+    (console.log("HERE IS THE FINAL ANSWER " + finalAnswer));
+    displayBlankSpaces();
+    console.log("here are the blanks for this round" + hiddenArray);
 
 
 
-// ------------- user presses a key-------------
+    // ------------- user presses a key-------------
 
 
 
-document.onkeyup = function (whatKey) {
+    document.onkeyup = function (whatKey) {
 
-    // when key is pressed, stored this value in var, userGuess ----  
-    let userGuess = whatKey.key;
-    console.log(userGuess);
+        // when key is pressed, stored this value in var, userGuess ----  
+        let userGuess = whatKey.key;
+        console.log(userGuess);
 
-    // checking the key pressed is a letter key-------
-    if ((whatKey.keyCode >= 65) && (whatKey.keyCode <= 90)) {
 
-        guessesLeft--;
+        //---------- function to put correct Letter in place
+        let placeLetter = function () {
+            let answerSplit = finalAnswer.split("");
+            let letterIndexInAnswer = answerSplit.indexOf(userGuess);
+            // let hiddenArray = blanks;
 
-        //----- these conditions depend on what key was picked vs finalAnswer---------
-        // users key matches key in answer-----
-        if ((guessesLeft >= 0) && (finalAnswer.includes(userGuess))) {
+            hiddenArray.splice(letterIndexInAnswer, 1, userGuess);
+
+            document.querySelector('.outputArray').textContent = hiddenArray;
+
+        }
+        // checking the key pressed is a letter key-------
+        if ((whatKey.keyCode >= 65) && (whatKey.keyCode <= 90)) {
+
+            guessesLeft--;
+
+            //----- these conditions depend on what key was picked vs finalAnswer---------
+            // users key matches key in answer-----
+            if ((guessesLeft >= 0) && (finalAnswer.includes(userGuess))) {
 
             /* display this key in appropriate space in HTML*/;
-            placeLetter();
-            }
+                placeLetter();
 
-            // USER WINS-------
-            if ((hiddenAnswer.includes("__")) = false) {
-                alert ("you won");
-                wins++;
-                guessesLeft = 15;
-                guessesSoFar = [];
-                generateAnswer();
+
+                // USER WINS-------
+                if ((hiddenArray.includes("__ ")) === false) {
+                    alert("you won");
+                    wins++;
+                    guessesLeft = 15;
+                    guessesSoFar = [];
+                    answer();
+                }
             }
 
             // user wrong 
-            else if ((guessesLeft > 0) && ((finalAnswer.includes(userGuess)) = false)) {
+            else if ((guessesLeft > 0) && ((finalAnswer.includes(userGuess)) === false)) {
                 guessesSoFar.push(userGuess); /*amend the guess to the guessesSoFar*/
             }
 
@@ -122,11 +105,28 @@ document.onkeyup = function (whatKey) {
 
             // user loses
             else {
-                losses++;
-                guessesLeft = 9;
+                guessesLeft = 15;
                 guessesSoFar = [];
-                console.log("how many times i lost" + losses);
-                generateAnswer();
+                answer();
             }
         }
     }
+
+    // connect to HTML so stats are viewable
+    let winsText = document.getElementById("wins");
+    let guessesLeftText = document.getElementById("guesses-left");
+    let guessesSoFarText = document.getElementById("guesses-so-far");
+    let hiddenArrayText = document.getElementsByClassName("outputArray");
+
+
+
+    // html display
+
+    winsText.textContent = "Wins: " + wins;
+    guessesLeftText.textContent = "Guesses left: " + guessesLeft;
+    guessesSoFarText.textContent = "Guesses So far: " + guessesSoFar;
+    hiddenArrayText.textContent = hiddenArray;
+
+
+
+}
